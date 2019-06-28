dd5aec7661c3b30c587f91b30bfdccfda336261e
Felix Seip <felixseip01@gmail.com>
2019-06-28 09:03:50 +0200

    Added functionality to retrieve time from NTP server

--- word-clock/word-clock.ino
+++ word-clock/word-clock.ino
@@ -7,6 +7,7 @@
 #include <WiFiNINA.h>
 #include <FastLED.h>
 #include <aREST.h>
+#include <WiFiUdp.h>
 
 #include "ClockElement.h"
 
@@ -26,10 +27,15 @@ String CLOCK_TYPE = "word-clock";
 String ROOM_NAME = "Wohnzimmer"; //Needs to be configurable
 int status = WL_IDLE_STATUS;
 
+unsigned int localPort = 2390;  
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
 WiFiServer server(80);
 WiFiClient client;
+WiFiUDP udp;
 
-char ssid[] = "Seip"; //Needs to be configurable
+char ssid[] = "Honor 7X"; //Needs to be configurable
 char pass[] = "connect.me"; //Needs to be configurable
 
 ClockElement timeClockElements[NUM_CLOCK_ELEMENTS];
@@ -95,7 +101,10 @@ void setup()
   status = WiFi.begin(ssid, pass);
   
   server.begin();
+  udp.begin(localPort);
   // you're connected now, so print out the status:
+
+  Serial.println(WiFi.localIP());
   // printWifiStatus();
 
   FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS);
@@ -111,7 +120,6 @@ void loop()
 {  
   String clientRequest = "";
   client = server.available();   // listen for incoming clients
-  getIPTime();
   handleClockFunctions();
     
   while (client.connected()) {
@@ -122,12 +130,11 @@ void loop()
 }
 
 void handleClockFunctions() {
-  int minutes = 45;
-  int hours = 10;
+  getTimeFromNTPServer();
   resetAllLEDs();
   showBasicClockElements();
-  showMinuteLEDs(minutes, hours, showUhrWord);
-  showHourLEDs(hours);
+  showMinuteLEDs(currentMinute, currentHour, showUhrWord);
+  showHourLEDs(currentHour);
     
   if (showUhrWord)
   {
@@ -141,10 +148,67 @@ void handleClockFunctions() {
   FastLED.show();
 }
 
-void getIPTime() {
-    unsigned long epoch = 1561488487 + 7200;
-    currentHour = ((epoch  % 86400L) / 3600);
-    currentMinute = (epoch  % 3600) / 60;  
+/* Don't hardwire the IP address or we won't get the benefits of the pool.
+    Lookup the IP address for the host name instead */
+//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "1.de.pool.ntp.org";
+
+void getTimeFromNTPServer(){
+    //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP);
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(2000);
+
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+  } else {
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    const unsigned long seventyYears = 2208988800UL;
+    unsigned long epoch = secsSince1900 - seventyYears + 7200;
+
+    currentHour  = (epoch  % 86400L) / 3600;
+    // print the hour, minute and second:
+    currentMinute = (epoch  % 3600) / 60;
+    Serial.print(currentHour);
+    Serial.print(":");
+    Serial.print(currentMinute);
+    Serial.println("");
+  }
+  // wait ten seconds before asking for the time again
+  delay(3000);
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress& address) {
+  Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
 
 int beginWifiServer(String command) {
